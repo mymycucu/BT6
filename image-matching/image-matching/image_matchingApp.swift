90a46bd464a0ee7c7a10645d5d68dcf729df7572
ariosyahputra <ariosyh@gmail.com>
2023-10-06 11:10:46 +0700

    init project

---
+++ image-matching/image-matching/image_matchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_matchingApp.swift
+//  image-matching
+//
+//  Created by Ario Syahputra on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct image_matchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
