391c4b3ae72d283385673f17db204b1de5bc6bc2
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-10-10 01:01:51 +0700

    test

---
+++ WordDragDrop/LearningModule.swift
@@ -0,0 +1,18 @@
+//
+//  LearningModule.swift
+//  WordDragDrop
+//
+//  Created by Sarah Uli Octavia on 10/10/23.
+//
+
+import SwiftUI
+
+struct LearningModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearningModule()
+}
---
+++ WordDragDrop/Model/Word.swift
@@ -0,0 +1,8 @@
+//
+//  Word.swift
+//  WordDragDrop
+//
+//  Created by Sarah Uli Octavia on 10/10/23.
+//
+
+import Foundation
