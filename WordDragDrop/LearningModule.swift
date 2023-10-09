86d9b3ed7df6fa71d174531412da5fe7bb1955d9
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-10-10 03:00:08 +0700

    ver 1

--- WordDragDrop/LearningModule.swift
+++ WordDragDrop/LearningModule.swift
@@ -13,23 +13,126 @@ struct LearningModule: View {
     @State var progress: CGFloat = 0
     @State var words: [Word] = words_
     
+    // MARK: Custom Grid Arrays
+    //drag words
+    @State var shuffledWords: [[Word]] = []
+    //drop
+    @State var rows: [[Word]] = []
+    
+    //animate wrong text dropped
+    @State var animateWrongText: Bool = false
+    @State var droppedCount: CGFloat = 0
     var body: some View {
         VStack(spacing:30){
             NavBar()
-            Rectangle()
-                .fill(Color.orange)
-                .frame(height: 200)
-                .cornerRadius(20)
+            HStack(spacing: 25){
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(height: 250)
+                    .cornerRadius(20)
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(height: 250)
+                    .cornerRadius(20)
+            }
+            .padding(30)
+            DropArea()
+                .padding(.vertical,30)
+            DragArea()
             Spacer()
         }
         .padding(30)
+        .onAppear{
+            if rows.isEmpty{
+                words = words.shuffled()
+                shuffledWords = generatingGrid()
+                words = words_
+                rows = generatingGrid()
+            }
+        }
+        .offset(x: animateWrongText ? -30 : 0)
     }
     
     // MARK: Drag Area
     @ViewBuilder
     func DragArea() -> some View{
         VStack(spacing: 12){
-            
+            ForEach(shuffledWords, id:\.self){ row in
+                HStack(spacing: 10){
+                    ForEach(row){ item in
+                        Text(item.value)
+                            .font(.title)
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, item.padding)
+                            .background{
+                                RoundedRectangle(cornerRadius: 6, style: .continuous)
+                                    .stroke(.gray)
+                            }
+                            .background{
+                                RoundedRectangle(cornerRadius: 6, style: .continuous)
+                                    .fill(item.isShowing ?  .gray.opacity(0.25): .clear)
+                            }
+                            .onDrag{
+                                return .init(contentsOf: URL(string: item.id))!
+                            }
+                    }
+                }
+                if shuffledWords.last != row {
+                    Divider()
+                }
+                
+            }
+        }
+    }
+    
+    // MARK: Drop Area
+    @ViewBuilder
+    func DropArea() -> some View{
+        VStack(spacing: 12){
+            ForEach($rows, id:\.self){ $row in
+                HStack(spacing: 10){
+                    ForEach($row){ $item in
+                        Text(item.value)
+                            .font(.title)
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, item.padding)
+                            .opacity(item.isShowing ? 1:0 )
+                            .background{
+                                RoundedRectangle(cornerRadius: 6, style: .continuous)
+                                    .fill(item.isShowing ? .clear : .gray.opacity(0.25))
+                            }
+                            .background{
+                                //if item is dropped
+                                RoundedRectangle(cornerRadius: 6, style: .continuous)
+                                    .stroke(.gray)
+                            }
+                            .onDrop(of: [.url], isTargeted: .constant(false)){
+                                providers in
+                                
+                                if let first = providers.first{
+                                    let _ = first.loadObject(ofClass: URL.self) { value, error in
+                                        guard let url = value else {return}
+                                        if item.id == "\(url)"{
+                                            withAnimation {
+                                                droppedCount += 1
+                                                let progress = (droppedCount/CGFloat(words.count))
+                                                item.isShowing = true
+                                                updateShuffledWords(word: item)
+                                            }
+                                        } else {
+                                            animateView()
+                                        }
+                                    }
+                                }
+                                
+                                return false
+                            }
+                    }
+                }
+                if rows.last != row {
+                    Divider()
+                }
+            }
         }
     }
     
@@ -68,7 +171,78 @@ struct LearningModule: View {
         }
     }
     
+    // MARK: Generating Custom Grid Columns
+    func generatingGrid() -> [[Word]]{
+        //Identifying each words width and updating into state var
+        for item in words.enumerated(){
+            let textSize = textSize(word: item.element)
+            
+            words[item.offset].textSize = textSize
+        }
+        
+        var gridArray: [[Word]] = []
+        var tempArray: [Word] = []
+        
+        //Current Width
+        var currentWidth: CGFloat = 0
+        // -30 -> Horizontal padding
+        let totalScreenWidth: CGFloat = UIScreen.main.bounds.width - 30
+        
+        for word in words{
+            currentWidth += word.textSize
+            
+            if currentWidth < totalScreenWidth {
+                tempArray.append(word)
+            }else{
+                gridArray.append(tempArray)
+                tempArray = []
+                currentWidth = word.textSize
+                tempArray.append(word)
+            }
+        }
+        //check
+        if !tempArray.isEmpty{
+            gridArray.append(tempArray)
+        }
+        
+        return gridArray
+    }
     
+    // MARK: Identifying Text Size
+    func textSize (word: Word) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: word.fontSize)
+        
+        let attributes = [NSAttributedString.Key.font : font]
+        
+        let size = (word.value as NSString).size(withAttributes: attributes)
+        
+        //Horizontal padding
+        return size.width + (word.padding * 2)
+    }
+    
+    // MARK: Updating Shuffled Words
+    func updateShuffledWords(word: Word){
+        for index in shuffledWords.indices{
+            for subIndex in shuffledWords[index].indices{
+                if shuffledWords[index][subIndex].id == word.id{
+                    shuffledWords[index][subIndex].isShowing = true
+                }
+            }
+        }
+    }
+    
+    // MARK: Animating View When Wrong Text Dropped
+    func animateView() {
+        withAnimation(.interactiveSpring(response: 0.3, dampingFraction: 0.2, blendDuration:0.2)){
+            animateWrongText = true
+        }
+       
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){
+            withAnimation(.interactiveSpring(response: 0.3, dampingFraction: 0.2, blendDuration:0.2)){
+                animateWrongText = false
+            }
+        }
+    }
     
 }
 
