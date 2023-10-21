e4a2ceec91dfbfbd95a99887254ced899c59d8cc
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-10-21 23:10:02 +0700

    update drag n drop

---
+++ WordDragDrop/View/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  WordDragDrop
+//
+//  Created by Sarah Uli Octavia on 16/10/23.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
