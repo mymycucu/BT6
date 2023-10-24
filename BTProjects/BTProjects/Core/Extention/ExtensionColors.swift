21569cde77510911c4a4244dd20716979848a5e6
ariosyahputra <ariosyh@gmail.com>
2023-10-24 12:37:50 +0700

    Added brand colors & custom fonts

---
+++ BTProjects/BTProjects/Core/Extention/ExtensionColors.swift
@@ -0,0 +1,8 @@
+//
+//  ExtensionColors.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 24/10/23.
+//
+
+import Foundation
---
+++ BTProjects/BTProjects/Core/Extention/ExtensionFonts.swift
@@ -0,0 +1,8 @@
+//
+//  ExtensionFonts.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 24/10/23.
+//
+
+import Foundation
