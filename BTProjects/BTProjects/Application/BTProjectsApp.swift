090fa47bd41978aec708dde205527a6285ab78a1
ariosyahputra <ariosyh@gmail.com>
2023-10-24 13:00:34 +0700

    Revert "Styleguide dev (#2)"
    This reverts commit 5195dabac7d54be648a5c5d11b3c45f2742416df.

---
+++ BTProjects/BTProjects/Application/BTProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTProjectsApp.swift
+//  BTProjects
+//
+//  Created by Hilmy Noerfatih on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BTProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LearningModuleView()
+        }
+    }
+}
