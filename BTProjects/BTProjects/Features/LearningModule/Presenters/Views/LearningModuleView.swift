94172978fcd134df1f2a7e24a4169f14f76e89fc
ariosyahputra <ariosyh@gmail.com>
2023-11-06 17:31:57 +0700

    Story view, video implementation, fix video blockers

--- BTProjects/BTProjects/Features/LearningModule/Presenters/Views/LearningModuleView.swift
+++ BTProjects/BTProjects/Features/LearningModule/Presenters/Views/LearningModuleView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct LearningModuleView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        StoryView()
     }
 }
 
---
+++ BTProjects/BTProjects/Features/LearningModule/Presenters/Views/StoryView.swift
@@ -0,0 +1,39 @@
+//
+//  StoryView.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 25/10/23.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        
+        ZStack {
+            
+            StoryBackground()
+                .padding(.top,20)
+                
+            VStack {
+                
+                StoryTopBar()
+                    .padding(.horizontal, 30)
+                
+                VideoContainer()
+                        .offset(x: 330, y: 0)
+                        .padding(.vertical, 80)
+                
+                StoryBottomBar()
+                    .padding(5)
+            }
+            .padding(.top, 30)
+            
+        }
+    
+    }
+}
+
+#Preview {
+    StoryView()
+}
