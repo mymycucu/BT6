edb29d0ef3c50271afa37090ae6e539b010e060f
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-11-07 09:24:52 +0700

    update: - add component (not sync yet) - animate menu category
    TBC:
    - sync component

---
+++ BTProjects/BTProjects/Features/Home/Components/CategoryButton.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryButton.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 06/11/23.
+//
+
+import SwiftUI
+
+struct CategoryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryButton()
+}
---
+++ BTProjects/BTProjects/Features/Home/Components/CategoryMenu.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryMenu.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 06/11/23.
+//
+
+import SwiftUI
+
+struct CategoryMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryMenu()
+}
