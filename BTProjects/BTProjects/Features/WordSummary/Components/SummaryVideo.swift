70cce57436ece4418ae7cfad14af53b5972ed3d0
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-11-08 14:22:01 +0700

    Update : - fix summary video - add replay button style & fix the file - delete video container - update category menu but not sync yet

---
+++ BTProjects/BTProjects/Features/WordSummary/Components/SummaryVideo.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryVideo.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 08/11/23.
+//
+
+import SwiftUI
+
+struct SummaryVideo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryVideo()
+}
