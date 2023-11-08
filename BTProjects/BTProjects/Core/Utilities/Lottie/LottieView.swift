f937fb6e2b1d3c8c449a856bd5f06a1d35c97ecf
ariosyahputra <ariosyh@gmail.com>
2023-11-08 18:10:59 +0700

    * added lottie & animation * added correct overlay

---
+++ BTProjects/BTProjects/Core/Utilities/Lottie/LottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LottieView.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 08/11/23.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LottieView()
+}
---
+++ BTProjects/BTProjects/Features/LearningModule/Presenters/Components/Tirukan/TirukanCorrect.swift
@@ -0,0 +1,18 @@
+//
+//  CorrectSLView.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 08/11/23.
+//
+
+import SwiftUI
+
+struct CorrectSLView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CorrectSLView()
+}
