9baa1aac5618eeb74ef002a7c8e14c43b5300dbf
ariosyahputra <ariosyh@gmail.com>
2023-11-09 10:08:42 +0700

    * added case state * optimize responsive

---
+++ BTProjects/BTProjects/Features/LearningModule/Presenters/Components/CaseFlow/CorrectState.swift
@@ -0,0 +1,18 @@
+//
+//  CorrectState.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 09/11/23.
+//
+
+import SwiftUI
+
+struct CorrectState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CorrectState()
+}
