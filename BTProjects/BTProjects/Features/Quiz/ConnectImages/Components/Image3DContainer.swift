94a489ff8a7414cf4948f19268ad86a89130f806
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-11-10 11:29:27 +0700

    Update: - add Image3DContainer - fix connectimage but still not working - fix summary video - try background

---
+++ BTProjects/BTProjects/Features/Quiz/ConnectImages/Components/Image3DContainer.swift
@@ -0,0 +1,18 @@
+//
+//  Image3DContainer.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 09/11/23.
+//
+
+import SwiftUI
+
+struct Image3DContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Image3DContainer()
+}
