28f7875453ffa2f2211156386f2649e0cfd38692
ariosyahputra <ariosyh@gmail.com>
2023-11-10 15:53:51 +0700

    * added multiple choice components

---
+++ BTProjects/BTProjects/Features/LearningModule/Presenters/Components/MultipleChoice/MultipleChoiceVideo.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleChoiceVideo.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 09/11/23.
+//
+
+import SwiftUI
+
+struct MultipleChoiceVideo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleChoiceVideo()
+}
