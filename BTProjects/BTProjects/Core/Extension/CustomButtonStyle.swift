eba48ab901043c8f0442805d69f86a4ab30641b2
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-11-12 09:24:23 +0700

    Update: - fixing the interaction, but selected not working for just one for both stories and questions

---
+++ BTProjects/BTProjects/Core/Extension/CustomButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  CustomButtonStyle.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 12/11/23.
+//
+
+import Foundation
---
+++ BTProjects/BTProjects/Features/ListOfPages/Components/StoryHeader.swift
@@ -0,0 +1,18 @@
+//
+//  StoryHeader.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 12/11/23.
+//
+
+import SwiftUI
+
+struct StoryHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryHeader()
+}
