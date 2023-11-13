75ede6222d56a5c235fdc5531c37026386f533b9
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-11-13 23:45:49 +0700

    Updates: - fix header, footer, background - fix padding features views - delete unused files

---
+++ BTProjects/BTProjects/Features/LearningModule/Components/Background.swift
@@ -0,0 +1,18 @@
+//
+//  Background.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 13/11/23.
+//
+
+import SwiftUI
+
+struct Background: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Background()
+}
---
+++ BTProjects/BTProjects/Features/LearningModule/Views/WordSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  WordSummaryView.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 13/11/23.
+//
+
+import SwiftUI
+
+struct WordSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WordSummaryView()
+}
