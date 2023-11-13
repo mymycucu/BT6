bae7129fb57d91889e1c89715dca084fea1504d6
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-11-13 19:30:29 +0700

    Updates: - add components header & footer - delete unused file - fix story background

---
+++ BTProjects/BTProjects/Features/LearningModule/Components/Footer.swift
@@ -0,0 +1,18 @@
+//
+//  Footer.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 13/11/23.
+//
+
+import SwiftUI
+
+struct Footer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Footer()
+}
---
+++ BTProjects/BTProjects/Features/LearningModule/Views/ConnectImagesView.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectImagesView.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 13/11/23.
+//
+
+import SwiftUI
+
+struct ConnectImagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectImagesView()
+}
