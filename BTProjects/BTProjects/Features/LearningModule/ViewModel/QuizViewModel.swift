a8fe53b596ce85ad5920f9d54b77ef2fc6e1a7c8
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-11-16 22:52:45 +0700

    updates : - add data for question connecting images
    TBC :
    - using coredata for ConnectingImages view

--- BTProjects/BTProjects/Features/LearningModule/ViewModel/QuizViewModel.swift
+++ BTProjects/BTProjects/Features/LearningModule/ViewModel/QuizViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+class quizViewModel : ObservableObject {
+    //MARK: save array of images
+    @Published var leftImages: [String] = []
+    @Published var rightImages: [String] = []
+    
+    //MARK: Get All Left & Right Images
+    func getLeftImages(question: Quest) -> [String]{
+        var allLeftImages: [String] = []
+        for item in question.answers {
+            allLeftImages.append(item.illustration)
+        }
+        return allLeftImages
+    }
+    
+    func getRightImages(question: Quest) -> [String]{
+        var allRightImages: [String] = []
+        for item in question.answers {
+            allRightImages.append(item.illustration2)
+        }
+        return allRightImages
+    }
+    
+    
+    //MARK: Check Answers -> Bool
+    func checkedAnswers(leftImage: String, rightImage: String, question: Quest) -> Bool {
+        
+        for item in question.answers {
+            if item.illustration == leftImage && item.illustration2 == rightImage {
+                
+                return true
+                
+            }
+        }
+        return false
+        
+    }
+}
