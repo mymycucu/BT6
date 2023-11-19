140581c7655614e7c9ee5445e319b1da99eb4506
ariosyahputra <ariosyh@gmail.com>
2023-11-19 21:22:58 +0700

    -adjusted home header to integrate with getTime - added getTime extension to give greeting based on current time

---
+++ BTProjects/BTProjects/Core/Extension/GetTime.swift
@@ -0,0 +1,18 @@
+//
+//  GetTime.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 19/11/23.
+//
+
+import SwiftUI
+
+struct GetTime: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GetTime()
+}
