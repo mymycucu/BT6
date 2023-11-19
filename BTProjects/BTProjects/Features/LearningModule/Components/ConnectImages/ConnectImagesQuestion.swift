4b3e0624dbd15a4e15d030cb8e36da8835440213
sarahoctav <sarahoctavia@student.ub.ac.id>
2023-11-19 15:06:43 +0700

    Updates: - fix exit layout - add connect images question modal - add storypage illus

---
+++ BTProjects/BTProjects/Features/LearningModule/Components/ConnectImages/ConnectImagesQuestion.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectImagesQuestion.swift
+//  BTProjects
+//
+//  Created by Sarah Uli Octavia on 19/11/23.
+//
+
+import SwiftUI
+
+struct ConnectImagesQuestion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectImagesQuestion()
+}
