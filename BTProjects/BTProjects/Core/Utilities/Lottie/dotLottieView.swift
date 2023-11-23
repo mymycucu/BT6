8f3664e80dc4e74b8ea37d9acfae378a2946cb1a
ariosyahputra <ariosyh@gmail.com>
2023-11-23 15:47:07 +0700

    - switched animation to giffy from lottie - adjusted gif files

---
+++ BTProjects/BTProjects/Core/Utilities/Lottie/dotLottieView.swift
@@ -0,0 +1,18 @@
+//
+//  dotLottieView.swift
+//  BTProjects
+//
+//  Created by Ario Syahputra on 22/11/23.
+//
+
+import SwiftUI
+
+struct dotLottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dotLottieView()
+}
